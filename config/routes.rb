e99b3c380250087792470c39a3ab5c882362308c
aubry prieur <aubry.prieur@gmail.com>
2020-04-07 18:33:23 +0200

    Tested Sign Up with Postman, refactor registrations controller, debugged routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  namespace :api, defaults: {format: json} do
+  namespace :api, defaults: {format: :json} do
     namespace :v1 do
       devise_scope :user do
         post "sign_up", to: "registrations#create"
