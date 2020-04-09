4e9c6eb10e6b56c3bab7e33b72c85b87a295abb5
aubry prieur <aubry.prieur@gmail.com>
2020-04-09 16:29:36 +0200

    Updated koala.rb

--- config/initializers/koala.rb
+++ config/initializers/koala.rb
@@ -1,8 +1,6 @@
 Koala.configure do |config|
-  config.app_id = MY_APP_ID
-  config.app_secret = MY_APP_SECRET
+  config.app_id = Rails.application.credentials.my_app_id
+  config.app_secret = Rails.application.credentials.my_app_secret
   # See Koala::Configuration for more options, including details on how to send requests through
   # your own proxy servers.
 end
-
-#
