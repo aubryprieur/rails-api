6fcbbcfd131314091725cbc48c3043c157d71de6
aubry prieur <aubry.prieur@gmail.com>
2020-04-10 10:14:41 +0200

    debugged authenticate.rb and created book and review model

--- app/controllers/concerns/authenticate.rb
+++ app/controllers/concerns/authenticate.rb
@@ -1,7 +1,8 @@
 module Authenticate
   def current_user
     auth_token = request.headers["AUTH-TOKEN"]
-    return unless auth_token@current_user = User.find_by authentication_token
+    return unless auth_token
+    @current_user = User.find_by authentication_token
   end
 
   def authenticate_with_token!
